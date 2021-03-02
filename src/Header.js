4a80e7d6453a1bed68daa89b9561c70ab842e27d
antoni perez <antoni.protoni@gmail.com>
2021-03-01 23:06:27 -0300

    first commit

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Fluid jumbotron</h1>
+        <p class="lead">
+          This is a modified jumbotron that occupies the entire horizontal space
+          of its parent.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+class titulo extends React.Component {
+  render() {
+    return;
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Fluid jumbotron</h1>
+        <p class="lead">
+          This is a modified jumbotron that occupies the entire horizontal space
+          of its parent.
+        </p>
+      </div>
+    </div>;
+  }
+}
+
+export default Header;
